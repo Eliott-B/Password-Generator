c6fe238edaa06f081e45c587c005ae0ae9b9f544
Eliott <45932754+Eliott-B@users.noreply.github.com>
2022-04-08 18:48:29 +0200

    Update Password_Generator.py

--- Password_Generator.py
+++ Password_Generator.py
@@ -5,27 +5,29 @@
 """
 from secrets import choice
 
-def Create_Password()->str:
+
+def Create_Password() -> str:
     """Créer un mot de passe composé de 16 caractères dont au moins un caracère spécial,
     un chiffre, une lettre minuscule et une lettre majuscule."""
     password = ""
     for i in range(16):
-        password += choice([chr(i) for i in range(33,126)])
-    if Verify_Password(password) == True:
+        password += choice([chr(i) for i in range(33, 126)])
+    if Verify_Password(password):
         return password
     elif Verify_Password(password) == False:
-        return Create_Password
+        return Create_Password()
 
-def Verify_Password(password:str)->bool:
+
+def Verify_Password(password: str) -> bool:
     """Retourne True si le mot de passe est complet."""
     c_speciaux = False
     c_chiffre = False
     c_majuscule = False
     c_minuscule = False
     speciaux = [chr(i) for i in range(33,47)] + [chr(j) for j in range(58,64)] + [chr(k) for k in range(91,96)] + [chr(123)] + [chr(m) for m in range(125,126)]
-    majuscule = [chr(i) for i in range(65,90)]
-    minuscule = [chr(i) for i in range(97,122)]
-    chiffre = [chr(i) for i in range(48,57)]
+    majuscule = [chr(i) for i in range(65, 90)]
+    minuscule = [chr(i) for i in range(97, 122)]
+    chiffre = [chr(i) for i in range(48, 57)]
     for elt in password:
         for k in speciaux:
             if elt == k:
@@ -39,7 +41,7 @@ def Verify_Password(password:str)->bool:
         for o in chiffre:
             if elt == o:
                 c_chiffre = True
-    if c_speciaux == True and c_majuscule == True and c_minuscule == True and c_chiffre == True:
+    if c_speciaux and c_majuscule and c_minuscule and c_chiffre:
         return True
     else:
         return False
